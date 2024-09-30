76eb708d8729264e1768e2ce54743809450ee2bc
Agustin Benoit <benoit.agustin@gmail.com>
2024-09-30 20:08:19 -0300

    Fix typo

--- tfbackendcfg.sh
+++ tfbackendcfg.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#TFBUCKET="terraform-state-bucket-2358974"
-REGION="us-east-1"
+read -p "Ingrese el nombre del bucket a crear: " TFBUCKET
+read -p "Ingrese la región (por ejemplo, us-east-1): " REGION
 
-read -p "Ingrese nombre del bucket a crear: " TFBUCKET
-read -p "Ingrese region: " REGION
+echo "Nombre del bucket: $TFBUCKET"
+echo "Región: $REGION"
 
-echo $TFBUCKET $REGION
+aws s3api create-bucket --bucket $TFBUCKET --region $REGION
 
-#aws s3api create-bucket --bucket $TFBUCKET --region $REGION
 
-#aws s3api put-bucket-versioning --bucket $TFBUCKET --versioning-configuration Status=Enabled
+aws s3api put-bucket-versioning --bucket $TFBUCKET --versioning-configuration Status=Enabled
 
-#aws dynamodb create-table \
-#    --table-name terraform-lock-table \
-#    --attribute-definitions AttributeName=LockID,AttributeType=S \
-#    --key-schema AttributeName=LockID,KeyType=HASH \
-#    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+aws dynamodb create-table \
+    --table-name terraform-lock-table \
+    --attribute-definitions AttributeName=LockID,AttributeType=S \
+    --key-schema AttributeName=LockID,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+echo "Bucket de S3 y tabla de DynamoDB creados correctamente."
