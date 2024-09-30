7fe4f69b9409fa6e7096968f630ebc0b3c6326f0
Agustin Benoit <benoit.agustin@gmail.com>
2024-09-30 20:08:39 -0300

    Agrego archivo outputs.tf

---
+++ environments/dev/simple-app1/outputs.tf
@@ -0,0 +1,19 @@
+output "private_subnet_ids" {
+  value       = data.terraform_remote_state.global.outputs.private_subnet_ids
+  description = "IDs de las subnets privadas donde se desplegarán las tareas Fargate"
+}
+
+output "ecs_service_security_group" {
+  value       = aws_security_group.ecs_service_sg.id
+  description = "Security Group ID para el servicio ECS"
+}
+
+output "alb_target_group_arn" {
+  value       = module.load_balancer.target_group_arn
+  description = "ARN del Target Group del Application Load Balancer"
+}
+
+output "alb_dns_name" {
+  value       = module.load_balancer.alb_dns_name
+  description = "DNS del Application Load Balancer"
+}
