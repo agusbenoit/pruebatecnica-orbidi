1e4ed742b71114ec97dbf2c94a7ca066f2d640da
Agustin Benoit <benoit.agustin@gmail.com>
2024-09-30 19:51:46 -0300

    Agrego variable REGION

--- environments/dev/_global/main.tf
+++ environments/dev/_global/main.tf
@@ -1,9 +1,13 @@
+locals {
+  aws_region = "REGION"
+}
+
 module "networking" {
   source              = "../../../modules/networking"
   vpc_cidr            = "10.0.0.0/16"
   private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"] 
-  availability_zones   = ["us-east-1a", "us-east-1b"]
+  availability_zones   = ["${local.aws_region}a", "${local.aws_region}b"]
   vpc_name             = "vpc-dev"
   tags = {
     Environment = "dev"
