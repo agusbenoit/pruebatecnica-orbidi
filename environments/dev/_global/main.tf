30ccb14cec6f174a7f60dfdcf40269069c1ea9b3
Agustin Benoit <benoit.agustin@gmail.com>
2024-09-30 18:39:49 -0300

    Creacion de VPC y ECS cluster para entorno DEV

---
+++ environments/dev/_global/main.tf
@@ -0,0 +1,16 @@
+module "networking" {
+  source              = "../../../modules/networking"
+  vpc_cidr            = "10.0.0.0/16"
+  private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"] 
+  availability_zones   = ["us-east-1a", "us-east-1b"]
+  vpc_name             = "vpc-dev"
+  tags = {
+    Environment = "dev"
+  }
+}
+
+module "ecs_cluster" {
+  source       = "../../../modules/ecs_cluster"
+  cluster_name = "ecs-cluster-dev"
+}
