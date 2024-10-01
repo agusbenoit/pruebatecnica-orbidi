95c33130978fe0fc7bc4e54d9f3578152ca84af7
Agustin Benoit <benoit.agustin@gmail.com>
2024-10-01 05:43:10 -0300

    Fix key de estado de terraform

--- environments/dev/simple-app2/backend.tf
+++ environments/dev/simple-app2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "BUCKETTERRAFORM"
-    key            = "dev/simple-app1/terraform.tfstate"
+    key            = "dev/simple-app2/terraform.tfstate"
     region         = "REGION"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
