ea51766be5cd03dad0cf69a7f477450e86a4c453
Agustin Benoit <benoit.agustin@gmail.com>
2024-10-01 04:10:58 -0300

    Script para inicializar la infraestructura y desplegar apps

---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+""
+set -e
+
+echo "Iniciando configuración del backend S3 y DynamoDB..."
+./tfbackendcfg.sh
+
+echo "Desplegando infraestructura global de Terraform..."
+cd environments/dev/_global
+terraform init
+terraform apply -auto-approve
+
+echo "Desplegando infraestructura de simple-app1..."
+cd ../simple-app1
+terraform init
+terraform apply -auto-approve
+
+echo "Desplegando infraestructura de simple-app2..."
+cd ../simple-app2
+terraform init
+terraform apply -auto-approve
+
+
+cd ../../../
+
+echo "Configurando ECR y roles para cada aplicación..."
+./pipelineconfig.sh
+
+echo "Iniciando el despliegue de simple-app1..."
+cd simple-app1
+./pipeline.sh
+
+echo "Iniciando el despliegue de simple-app2..."
+cd ../simple-app2
+./pipeline.sh
+
+echo "Despliegue completado con éxito."
