ece7263c97c7533b511a6e54a3c02cc5717c1f2d
Agustin Benoit <benoit.agustin@gmail.com>
2024-10-01 02:59:50 -0300

    Configuracion de recursos para simple-app2

--- environments/dev/simple-app2/main.tf
+++ environments/dev/simple-app2/main.tf
@@ -68,3 +68,93 @@ resource "aws_security_group" "ecs_service_sg" {
     Environment = "dev"
   }
 }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-${var.name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecsTaskExecutionRole-${var.name}"
+    Environment = "dev"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+resource "aws_ecs_task_definition" "simple-app1-task-definition" {
+  family                   = "${var.name}"
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([{
+    name  = "${var.name}"
+    image = "nginx:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 8000
+      hostPort      = 8000
+    }]
+  }])
+
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  lifecycle {
+    ignore_changes = [
+      container_definitions,
+      task_role_arn,
+      execution_role_arn,
+      requires_compatibilities,
+      cpu,
+      memory
+    ]
+  }
+}
+
+resource "aws_ecs_service" "simple_app1_service" {
+  name            = "${var.name}-service"
+  cluster         = data.terraform_remote_state.global.outputs.ecs_cluster_id
+  task_definition = "${var.name}" 
+  desired_count   = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = data.terraform_remote_state.global.outputs.private_subnet_ids
+    security_groups  = [aws_security_group.ecs_service_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = module.load_balancer.target_group_arn
+    container_name   = "${var.name}"
+    container_port   = 8000
+  }
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+
+  lifecycle {
+    ignore_changes = [
+      task_definition    #la definicion de la tarea va a ser manejada por el pipeline
+    ]
+  }
+
+  depends_on = [module.load_balancer.listener_arn]
+}
